bff2e419ced95465c76d45b84b44e9549fb91771
HARI VARDHAN <uhvardhan987@gmail.com>
2026-04-16 15:04:36 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -15,7 +15,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     if data == "about":
         await query.message.edit_text(
             text=(
-                f"<b>🤖 My Name :</b> <a href='https://t.me/FileSharingXProBot'>File Sharing Bot</a>\n"
+                f"<b>🤖 My Name :</b> <a href='https://t.me/km_botzs'>File Sharing Bot</a>\n"
                 f"<b>📝 Language :</b> <a href='https://python.org'>Python 3</a>\n"
                 f"<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a>\n"
                 f"<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>\n"
